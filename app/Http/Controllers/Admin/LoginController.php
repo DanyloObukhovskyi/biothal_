b29c62df2f9cf0b22244a702585638a74d629496
Nikita Miahkoy <niksgreek@gmail.com>
2021-04-28 00:30:06 +0300

    fix login page

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:web', ['except' => ['login']]);
+        $this->middleware('auth:web', ['except' => ['login','index']]);
     }
 
     public function index()
