8670a867ec86a98e73f884d8693110733c96dc97
Ivan1Gul <gulvanya9@gmail.com>
2021-05-28 17:36:34 +0300

    rm validate unique email and phone from distribution offer

--- app/Http/Requests/CreateOffer.php
+++ app/Http/Requests/CreateOffer.php
@@ -23,10 +23,8 @@ class CreateOffer extends FormRequest
             'name.max' => 'Имя должно содержать не более чем 40 символов',
             'name.string' => 'Имя должно только буквы',
             'email.required' => 'Вы не ввели електронную почту',
-            'email.unique' => 'Запрос с этой почтой уже был отправлен',
             'email.email' => 'Вы ввели некорректный адрес электронной почты',
             'phone.required' => 'Вы не ввели телефоный номер',
-            'phone.unique' => 'Запрос с этим номером телефона уже был отправлен',
             'text.max' => 'Сообщение должно содержать не более чем 1000 символов',
         ];
     }
@@ -40,8 +38,8 @@ class CreateOffer extends FormRequest
     {
         return [
             'name' => 'required|string|max:40',
-            'email' => 'required|email|unique:distribution_offer',
-            'phone' => 'required|max:20|unique:distribution_offer',
+            'email' => 'required|email',
+            'phone' => 'required|max:20',
             'text' => 'max:1000',
         ];
     }
