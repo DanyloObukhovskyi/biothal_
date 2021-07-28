1951545094ba87325da863a53f63ec1848d4fd5e
Nikita Miahkoy <niksgreek@gmail.com>
2021-07-28 11:56:58 +0300

    fix model

--- new-front/src/pages/Dashboard.vue
+++ new-front/src/pages/Dashboard.vue
@@ -33,6 +33,23 @@
                 var sn = d.getElementsByTagName(s)[0]; sn.parentNode.insertBefore(gcw, sn);
             })(document, window, 'script');
 
+            (function(d, w, s) {
+                var _tbEmbedArgs = _tbEmbedArgs || [];
+                (function () {
+                    var u =  "https://widget.textback.io/widget";
+                    _tbEmbedArgs.push(["widgetId", "7ddd6a62-c29f-4428-bf5e-15d61b00a032"]);
+                    _tbEmbedArgs.push(["baseUrl", u]);
+
+                    var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
+                    g.type = "text/javascript";
+                    g.charset = "utf-8";
+                    g.defer = true;
+                    g.async = true;
+                    g.src = u + "/widget.js";
+                    s.parentNode.insertBefore(g, s);
+                })();
+            })(document, window, 'script');
+
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
