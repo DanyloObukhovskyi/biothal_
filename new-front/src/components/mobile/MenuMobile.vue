1c952543bc2d42f3ddb00bf4d26bc6905940652c
Ivan1Gul <gulvanya9@gmail.com>
2021-08-11 12:22:55 +0300

    change menu text

--- new-front/src/components/mobile/MenuMobile.vue
+++ new-front/src/components/mobile/MenuMobile.vue
@@ -31,8 +31,9 @@
 
       <div style="background-color: black; height: 34px; color: white; font-size: 14px; text-align: center; display: flex; justify-content: center;" ref="app-bar-info">
 <!--          Заказы в которых есть "Крем Жиросжигающий Антицеллюлитный с охлаждающим эффектом" - отправляются в течении 7 рабочих дней.-->
-          <div style="margin-top: 7px;">Отправка заказов в течении 3-5 рабочих дней</div>
-          <div><img class="package-mobile" src="../../../public/package.svg"/></div>
+<!--          <div style="margin-top: 7px;">Отправка заказов в течении 3-5 рабочих дней</div>-->
+          <div style="margin-top: 7px;">Бесплатная доставка при заказе от 1500грн.</div>
+<!--          <div><img class="package-mobile" src="../../../public/package.svg"/></div>-->
       </div>
 
     <v-navigation-drawer
