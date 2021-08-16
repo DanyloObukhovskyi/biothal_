379c8f860fcd59e911e420ee848d77b34dc6db6a
Nikita Miahkoy <niksgreek@gmail.com>
2021-08-16 16:13:03 +0300

    add gtm

--- new-front/src/main.js
+++ new-front/src/main.js
@@ -13,6 +13,7 @@ import VueLodash from 'vue-lodash';
 import lodash from 'lodash';
 import VueLoading from 'vuejs-loading-plugin';
 import VueMeta from 'vue-meta';
+import VueGtm from '@gtm-support/vue2-gtm';
 
 axios.defaults.baseURL = process.env.VUE_APP_REQUEST_BASE_URL + process.env.VUE_APP_REQUEST_PREFIX;
 
@@ -79,6 +80,16 @@ Vue.use(VueLoading, {
     loading: false, // default false
     background: 'rgb(255,255,255)', // set custom background
     classes: ['loader_opacity'] // array, object or string
+})
+Vue.use(  VueGtm, {
+    id: "GTM-57NZVR4", // Your GTM single container ID, array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy'] or array of objects [{id: 'GTM-xxxxxx', queryParams: { gtm_auth: 'abc123', gtm_preview: 'env-4', gtm_cookies_win: 'x'}}, {id: 'GTM-yyyyyy', queryParams: {gtm_auth: 'abc234', gtm_preview: 'env-5', gtm_cookies_win: 'x'}}], // Your GTM single container ID or array of container ids ['GTM-xxxxxx', 'GTM-yyyyyy']
+    defer: false, // Script can be set to `defer` to speed up page load at the cost of less accurate results (in case visitor leaves before script is loaded, which is unlikely but possible). Defaults to false, so the script is loaded `async` by default
+    compatibility: false, // Will add `async` and `defer` to the script tag to not block requests for old browsers that do not support `async`// Will add `nonce` to the script tag
+    enabled: true, // defaults to true. Plugin can be disabled by setting this to false for Ex: enabled: !!GDPR_Cookie (optional)
+    debug: true, // Whether or not display console logs debugs (optional)
+    loadScript: true, // Whether or not to load the GTM Script (Helpful if you are including GTM manually, but need the dataLayer functionality in your components) (optional)
+    vueRouter: router, // Pass the router instance to automatically sync with router (optional)// Don't trigger events for specified router names (case insensitive) (optional)
+    trackOnNextTick: false, // Whether or not call trackView in Vue.nextTick
 })
 
 import '@/styles/main.scss';
