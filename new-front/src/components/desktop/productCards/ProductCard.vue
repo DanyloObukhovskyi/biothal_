95c617a7082eb585d09d79149a0363b65a35b406
Nikita Miahkoy <niksgreek@gmail.com>
2021-08-18 00:28:24 +0300

    check

--- new-front/src/components/desktop/productCards/ProductCard.vue
+++ new-front/src/components/desktop/productCards/ProductCard.vue
@@ -114,7 +114,7 @@ export default {
             value: this.dataCard.currency, currency: 'USD', content_ids: this.dataCard.id, content_type: 'product', content_category: this.dataCard.category,test_event_code: 'TEST60059'
         })
       this.$analytics.fbq.event('AddToCart', {
-          value: this.dataCard.currency, currency: 'USD', content_ids: this.dataCard.id, content_type: 'product', content_category: this.dataCard.category,test_event_code: 'TEST60059'
+          value: this.dataCard.currency, currency: 'USD', content_ids: this.dataCard.id, content_type: 'product', content_category: this.dataCard.category,test_event_code: TEST60059
       })
         console.log(this.$analytics)
       this.addProduct(product)
