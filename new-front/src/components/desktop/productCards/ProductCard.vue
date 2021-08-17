6a7938748738fb6c4f507c82ed1fcae844021b0e
Nikita Miahkoy <niksgreek@gmail.com>
2021-08-17 23:21:42 +0300

    fix product card

--- new-front/src/components/desktop/productCards/ProductCard.vue
+++ new-front/src/components/desktop/productCards/ProductCard.vue
@@ -116,6 +116,7 @@ export default {
       this.$analytics.fbq.event('track', 'AddToCart', {
           value: this.dataCard.currency, currency: 'USD', content_ids: this.dataCard.id, content_type: 'product', content_category: this.dataCard.category
       })
+        console.log($analytics)
       this.addProduct(product)
 
       this.action_data_basket_info(product);
