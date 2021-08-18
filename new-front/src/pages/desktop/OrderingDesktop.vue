f164df3e1522ca835870a50c56b7e9782b3426e9
Nikita Miahkoy <niksgreek@gmail.com>
2021-08-18 12:31:45 +0300

    fix facebook

--- new-front/src/pages/desktop/OrderingDesktop.vue
+++ new-front/src/pages/desktop/OrderingDesktop.vue
@@ -463,7 +463,7 @@
                         sum = (sum + (product.currency * product.quantity));
                     })
                     this.$analytics.fbq.event( 'InitiateCheckout', {
-                        value: sum, currency: 'USD', content_ids: productIds, content_type: 'product', content_category: product.category
+                        value: sum, currency: 'USD', content_ids: productIds, content_type: 'product'
                     })
 
                     this.clearValidation()
